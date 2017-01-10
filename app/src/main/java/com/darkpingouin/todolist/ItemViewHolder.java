096b4610d05e50eb26e7577ac544d1200e9293b1
orset_a <aurelie.orset@epitech.eu>
2017-01-10 20:28:54 +0100

    Debut de ListView

---
+++ app/src/main/java/com/darkpingouin/todolist/ItemViewHolder.java
@@ -0,0 +1,13 @@
+package com.darkpingouin.todolist;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by Oreo on 10/01/2017.
+ */
+ class TweetViewHolder{
+        public TextView Title;
+        public TextView text;
+        public ImageView dueDate;
+}
