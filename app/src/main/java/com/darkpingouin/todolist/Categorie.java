e3de00fc22bf6ec0e06e5e101496950111db45aa
orset_a <aurelie.orset@epitech.eu>
2017-01-25 15:21:50 +0100

    Categorie JavaDoc

--- app/src/main/java/com/darkpingouin/todolist/Categorie.java
+++ app/src/main/java/com/darkpingouin/todolist/Categorie.java
@@ -9,6 +9,11 @@ public class Categorie {
     private int color;
     private boolean show;
 
+    /**
+     * Constructeur
+     * @param n nom
+     * @param c couleur
+     */
     public Categorie(String n, int c)
     {
         this.name = n;
@@ -16,29 +21,54 @@ public class Categorie {
         show = true;
     }
 
+    /**
+     * Renvoie le nom de la catégorie
+     * @return nom de la catégorie
+     */
     public String getName()
     {
         return (this.name);
     }
 
+    /**
+     * Renvoies la couleur de la catégorie
+     * @return couleur de la catégorie
+     */
     public int getColor()
     {
         return (this.color);
     }
 
+    /**
+     * Set le nom de la catégorie
+     * @param n nom
+     */
     public void setName(String n)
     {
         this.name = n;
     }
 
+    /**
+     * Set la couleur
+     * @param c couleur
+     */
     public void setColor(int c)
     {
         this.color = c;
     }
 
+    /**
+     * Pemret de savoir si la catégorie doit être affichée
+     * @return boolean
+     */
     public boolean getShow() {
         return this.show;
     }
+
+    /**
+     * Set la catégorie en visible ou invible
+     * @param show1 true ou false
+     */
     public void setShow(boolean show1) {
         this.show = show1;
     }
